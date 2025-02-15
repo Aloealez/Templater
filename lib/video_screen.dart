6a1ec048da1ddecb6f349571bc2a28b835b4a452
Kalina <102462480+KalinaSta@users.noreply.github.com>
2025-02-15 11:00:29 +0100

    In the Ted-ed cinema there is an option to skip the video

--- lib/video_screen.dart
+++ lib/video_screen.dart
@@ -19,7 +19,10 @@ class VideoScreen extends StatefulWidget {
 }
 
 class _VideoScreenState extends State<VideoScreen> {
-  double _curtainPosition = 0;
+  double _curtainOffset = 0;
+  bool _curtainsInitialized = false;
+  double _screenWidth = 0;
+  bool _showSkipButton = false;
 
   final _controller = YoutubePlayerController(
     params: const YoutubePlayerParams(
@@ -30,6 +33,16 @@ class _VideoScreenState extends State<VideoScreen> {
       enableKeyboard: false,
     ),
   );
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_curtainsInitialized) {
+      _screenWidth = MediaQuery.of(context).size.width;
+      _curtainOffset = 0;
+      _curtainsInitialized = true;
+    }
+  }
 
   @override
   void initState() {
@@ -48,10 +61,15 @@ class _VideoScreenState extends State<VideoScreen> {
       }
     });
 
-    // Update curtain position after a delay
-    Future.delayed(Duration(milliseconds: 300), () {
+    Future.delayed(Duration(milliseconds: 4000), () {
       setState(() {
-        _curtainPosition = -MediaQuery.of(context).size.width;
+        _curtainOffset = -_screenWidth / 1.9;
+      });
+    });
+
+    Future.delayed(Duration(seconds: 6), () {
+      setState(() {
+        _showSkipButton = true;
       });
     });
   }
@@ -65,34 +83,29 @@ class _VideoScreenState extends State<VideoScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    double cinemaBarHeight = size.height * 0.08, cinemaBarAlignment = -0.8;
+    double cinemaBarHeight = size.height * 0.08;
+    double cinemaBarOffset = 0.0;
 
     return Scaffold(
-      appBar: appBar(
-        context,
-        "",
-      ),
+      appBar: appBar(context, ""),
       body: Center(
         child: Container(
-          margin: EdgeInsets.only(
-            left: 0,
-            right: 0,
-          ),
+          margin: EdgeInsets.zero,
           child: Stack(
             children: [
               Align(
-                alignment: Alignment(0, 0),
+                alignment: Alignment(0, -0.2),
                 child: YoutubePlayer(
                   controller: _controller,
                 ),
               ),
               Align(
-                alignment: Alignment(0, cinemaBarAlignment),
+                alignment: Alignment(0, -0.8),
                 child: SizedBox(
                   height: cinemaBarHeight,
                   width: size.width,
                   child: Container(
-                    color: Color(0xFF777799),
+                    color: const Color(0xFF777799),
                     child: Center(
                       child: Text(
                         "TedEd Cinema",
@@ -109,39 +122,78 @@ class _VideoScreenState extends State<VideoScreen> {
               ),
               AnimatedPositioned(
                 duration: Duration(milliseconds: 3000),
-                left: _curtainPosition-size.width/2,
-                top: 2*cinemaBarHeight + cinemaBarAlignment, // Start immediately after the "TedEd Cinema" bar
-                height: (size.height - cinemaBarHeight)/2, // Remaining screen height
+                left: _curtainOffset,
+                top: 2 * cinemaBarHeight + cinemaBarOffset,
+                width: size.width / 1.95,
+                height: (size.height - cinemaBarHeight) / 2,
                 curve: Curves.ease,
-                child: SizedBox(
-                  width: size.width, // Half screen width for left curtain
-                  child: Image.asset(
-                    "assets/video/curtainleftKalina.png",
-                    fit: BoxFit.contain,
-                  ),
+                child: Image.asset(
+                  "assets/video/curtainleftKalina.png",
+                  fit: BoxFit.contain,
                 ),
               ),
               AnimatedPositioned(
                 duration: Duration(milliseconds: 3000),
-                right: _curtainPosition-size.width/2,
-                top: 2*cinemaBarHeight + cinemaBarAlignment, // Start immediately after the "TedEd Cinema" bar
-                height: (size.height - cinemaBarHeight)/2, // Remaining screen height
+                right: _curtainOffset,
+                top: 2 * cinemaBarHeight + cinemaBarOffset,
+                width: size.width / 1.95,
+                height: (size.height - cinemaBarHeight) / 2,
                 curve: Curves.ease,
-                child: SizedBox(
-                  width: size.width, // Half screen width for right curtain
-                  child: Image.asset(
-                    "assets/video/curtainrightKalina.png",
-                    fit: BoxFit.contain,
-                  ),
+                child: Image.asset(
+                  "assets/video/curtainrightKalina.png",
+                  fit: BoxFit.contain,
                 ),
               ),
               Align(
                 alignment: Alignment(0, 0.8),
                 child: Transform.scale(
-                  scale: 2.0, // Double the size of the audience image
+                  scale: 2.0,
                   child: Image.asset(
                     "assets/video/audience.png",
                     fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment(0, 0.4),
+                child: AnimatedOpacity(
+                  opacity: _showSkipButton ? 1.0 : 0.0,
+                  duration: Duration(seconds: 1),
+                  child: SizedBox(
+                    height: size.height * 0.05,
+                    width: size.width * 0.75,
+                    child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.secondary,
+                        foregroundColor: Theme.of(context).colorScheme.onSecondary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.push(
+                          context,
+                          PageTransition(
+                            type: PageTransitionType.fade,
+                            child: widget.route,
+                          ),
+                        );
+                      },
+                      icon: Icon(
+                        Icons.skip_next,
+                        size: 30,
+                        color: Theme.of(context).colorScheme.onSecondary,
+                      ),
+                      label: Text(
+                        "Skip",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                          color: Theme.of(context).colorScheme.onSecondary,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
