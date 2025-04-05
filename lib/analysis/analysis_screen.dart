d920d3a59286623c9969f6db972051916be0b780
Uzair Arain <142334690+uzaiearain@users.noreply.github.com>
2025-04-05 16:59:03 +0500

    update analysis screen and add your progress screen

--- lib/analysis/analysis_screen.dart
+++ lib/analysis/analysis_screen.dart
@@ -1,270 +1,295 @@
+import 'package:brainace_pro/score_n_progress/score_axis.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
 import '/navbar.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'dart:math';
 import 'package:intl/intl.dart';
 import 'package:brainace_pro/buttons.dart';
-
+import 'yourProgress_screen.dart';
 
 class AnalysisScreen extends StatefulWidget {
-  // parametr percent steruje wyświetlaniem punktów w procentach lub jako zwykłe punkty
-  // userPoints wyniosłam do konstruktora, by można było podawać wynik z zewnątrz
-  final bool percent;
-  final double userPoints;
+  final double initScore;
+  final Map<String, dynamic> questions;
 
-  const AnalysisScreen({
-    super.key,
-    required this.percent,
-    required this.userPoints,
-  });
+  AnalysisScreen({required this.initScore, required this.questions});
 
   @override
-  State<AnalysisScreen> createState() => _AnalysisScreen();
+  _AnalysisScreenState createState() => _AnalysisScreenState();
 }
 
-class _AnalysisScreen extends State<AnalysisScreen> {
-  late double userLevel;
-  bool isTextVisible = false;
+late SharedPreferences prefs;
 
-
-  late List<_ChartData> data;
-  late TooltipBehavior _tooltip;
-
-  List<double> normalDistr(double start, double end, double step) {
-    double sigma = 1.5;
-    double mu = 0;
-
-    List<double> res = List.generate(
-      ((end - start + 1) / step).ceil(),
-          (index) {
-        double x = start + index * step;
-        return (1 / (sigma * sqrt(2 * pi))) *
-            exp((-1 * (x - mu) * (x - mu)) / (2 * sigma * sigma));
-      },
-    );
-    return res;
-  }
-
-  double getDailyUserLevel() {
-    final String today = DateFormat('yyyy-MM-dd').format(DateTime.now());
-    final int seed = today.hashCode;
-    final Random random = Random(seed);
-    return (random.nextInt(151) + 150) / 1000; // np. 0.15–0.30
-  }
-
+class _AnalysisScreenState extends State<AnalysisScreen> {
   @override
   void initState() {
     super.initState();
-    userLevel = getDailyUserLevel();
+    _initializePreferences();
+  }
 
-    // Opóźnione włączenie animacji z fade-in
-    Future.delayed(const Duration(milliseconds: 300), () {
-      setState(() {
-        isTextVisible = true;
-      });
+  String userScores = "0"; // Default value
+
+  Future<void> _initializePreferences() async {
+    prefs = await SharedPreferences.getInstance();
+    String? userScore =
+        (prefs != null ? prefs.getString("scores_questionsLast") : null) ?? "0";
+    setState(() {
+      userScores = userScore ?? "0";
     });
-
-    data = List.generate(
-      200,
-          (index) => _ChartData(
-        index.toString(),
-        normalDistr(-5, 5, 0.05)[index],
-      ),
-    );
-
-    _tooltip = TooltipBehavior(enable: false);
   }
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(
-          left: size.width / 10,
-          right: size.width / 10,
+        width: size.width,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 40),
+            buildTitle("Current Score Prediction", size),
+            const SizedBox(height: 20),
+            buildScoreContainer(size),
+            const SizedBox(height: 30),
+            buildTitle("Your Progress", size),
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) =>
+                        Progress(),
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                      return ScaleTransition(
+                        scale: Tween<double>(begin: 0.8, end: 1.0)
+                            .animate(animation),
+                        child: child,
+                      );
+                    },
+                  ),
+                );
+              },
+              child: buildProgressContainer(context, size),
+            ),
+            const SizedBox(height: 30),
+            buildTitle("Weak Points And Strengths", size),
+            const SizedBox(height: 20),
+            buildWeaknessContainer(),
+          ],
         ),
-        child: Stack(
-          children: <Widget>[
-            Align(
-              alignment: const Alignment(0, 0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      ),
+      bottomNavigationBar: MyBottomNavigationBar(),
+    );
+  }
+
+  Widget buildTitle(String title, Size size) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontSize: size.width / 20,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget buildScoreContainer(Size size) {
+    return Container(
+      width: containerWidth,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [Color(0xFF6A0DAD), Color(0xFF8A2BE2)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Text.rich(
+              TextSpan(
                 children: [
-                  SizedBox(
-                    height: size.height / 7,
-                    child: Column(
-                      children: [
-                        AnimatedOpacity(
-                          // Tekst "Great Job 🥳"
-                          opacity: isTextVisible ? 1.0 : 0.0,
-                          duration: const Duration(milliseconds: 300),
-                          child: Text(
-                            "Great Job 🥳",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: size.width / 12,
-                              color: Theme.of(context).colorScheme.primaryFixedDim,
-                            ),
-                          ),
-                        ),
-                        AnimatedOpacity(
-                          // Tekst "You got [x points]" lub "You got [x%]"
-                          opacity: isTextVisible ? 1.0 : 0.0,
-                          duration: const Duration(milliseconds: 300),
-                          child: Text(
-                            widget.percent
-                                ? "You got [${widget.userPoints.toStringAsFixed(1)}%]."
-                                : "You got [${widget.userPoints.toStringAsFixed(0)}] points.",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: size.width / 15,
-                              color: Theme.of(context).colorScheme.primaryFixedDim,
-                            ),
-                          ),
-                        ),
-                      ],
+                  TextSpan(
+                    text: userScores,
+                    style: TextStyle(
+                      fontSize: size.width / 10,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w800,
+                      fontFamily: "open sans",
                     ),
                   ),
-                  SizedBox(
-                    height: size.height / 5,
-                    child: SfCartesianChart(
-                      primaryXAxis: CategoryAxis(
-                        majorTickLines: const MajorTickLines(size: 0),
-                        axisLine: AxisLine(
-                          width: 2,
-                          color: Theme.of(context).colorScheme.primaryFixedDim,
-                        ),
-                        labelStyle: const TextStyle(
-                          fontSize: 0,
-                        ),
-                      ),
-                      primaryYAxis: NumericAxis(
-                        minimum: 0,
-                        maximum: 0.4,
-                        interval: 100,
-                        majorTickLines: const MajorTickLines(size: 0),
-                        axisLine: const AxisLine(
-                          width: 0,
-                        ),
-                        labelStyle: const TextStyle(
-                          fontSize: 0,
-                        ),
-                      ),
-                      tooltipBehavior: _tooltip,
-                      series: <CartesianSeries<_ChartData, String>>[
-                        AreaSeries<_ChartData, String>(
-                          dataSource: data
-                              .sublist(0, (data.length * userLevel).floor()),
-                          xValueMapper: (_ChartData data, _) => data.x,
-                          yValueMapper: (_ChartData data, _) => data.y,
-                          name: 'Gold',
-                          color: Theme.of(context)
-                              .colorScheme
-                              .secondaryContainer
-                              .withAlpha(79),
-                        ),
-                        LineSeries<_ChartData, String>(
-                          dataSource: data,
-                          xValueMapper: (_ChartData data, _) => data.x,
-                          yValueMapper: (_ChartData data, _) => data.y,
-                          name: 'Gold',
-                          color: Theme.of(context).colorScheme.primaryFixedDim,
-                          width: 2,
-                        ),
-                        LineSeries<_ChartData, String>(
-                          dataSource: [
-                            _ChartData(
-                              data[(data.length * userLevel).floor()].x,
-                              0,
-                            ),
-                            _ChartData(
-                              data[(data.length * userLevel).floor()].x,
-                              data[(data.length * userLevel).floor()].y,
-                            ),
-                          ],
-                          xValueMapper: (_ChartData data, _) => data.x,
-                          yValueMapper: (_ChartData data, _) => data.y,
-                          name: 'Gold',
-                          color: Theme.of(context).colorScheme.primaryFixedDim,
-                          width: 2,
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: size.height / 20,
-                  ),
-                  SizedBox(
-                    height: size.height / 4,
-                    width: size.width / 1.5,
-                    child: Column(
-                      children: [
-                        AnimatedOpacity(
-                          opacity: isTextVisible ? 1.0 : 0.0,
-                          duration: const Duration(milliseconds: 300),
-                          child: Text(
-                            "You got a better score than ${(userLevel * 100).toStringAsFixed(1)}% of the users.",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: size.width / 20,
-                              color: Theme.of(context).colorScheme.primaryFixedDim,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: size.height / 20,
-                        ),
-                        AnimatedOpacity(
-                          opacity: isTextVisible ? 1.0 : 0.0,
-                          duration: const Duration(milliseconds: 300),
-                          child: Text(
-                            "${(100 - (userLevel * 100)).toStringAsFixed(1)}% of the app users got a better score than you.",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: size.width / 20,
-                              color: Theme.of(context).colorScheme.primaryFixedDim,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: size.height / 20,
+                  TextSpan(
+                    text: "/ 1600 composite",
+                    style: TextStyle(fontSize: size.width / 16),
                   ),
                 ],
               ),
             ),
-            // Przycisk "Continue"
-            Positioned(
-              bottom: 50,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: SizedBox(
-                  width: size.width * 0.75,
-                  height: size.height * 0.05,
-                  child: RedirectButton(
-                    text: 'Continue',
-                    width: size.width,
-                  ),
+          ),
+          SizedBox(height: 8),
+          buildScoreText("0", "0", size),
+        ],
+      ),
+    );
+  }
+
+  Widget buildScoreText(String primary, String secondary, Size size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: primary,
+                style: TextStyle(
+                  fontSize: size.width / 10,
+                  fontWeight: FontWeight.w800,
                 ),
               ),
-            ),
+              TextSpan(
+                text: "\tMath",
+                style: TextStyle(fontSize: size.width / 16),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(width: 20),
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: secondary,
+                style: TextStyle(
+                  fontSize: size.width / 10,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              TextSpan(
+                text: "\tR&W",
+                style: TextStyle(
+                  fontSize: size.width / 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildProgressContainer(BuildContext context, Size size) {
+    return Container(
+      width: containerWidth,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [Color(0xFF6A0DAD), Color(0xFF8A2BE2)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        decoration: BoxDecoration(
+          border: Border(left: BorderSide(color: Colors.white, width: 2)),
+        ),
+        child: Column(
+          children: [
+            buildProgressBar(context, "Before", 500),
+            SizedBox(height: size.height / 40),
+            buildProgressBar(context, "Now", 160),
           ],
         ),
       ),
-      bottomNavigationBar: const MyBottomNavigationBar(),
+    );
+  }
+
+  Widget buildProgressBar(BuildContext context, String label, int score) {
+    final size = MediaQuery.of(context).size;
+    double progress = score / 1600;
+
+    return Container(
+      width: containerWidth,
+      height: size.height / 30,
+      child: Row(
+        children: [
+          Container(
+            width: containerWidth * progress,
+            height: size.height / 30,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.only(left: 8),
+            child: Text(
+              score.toString(),
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(color: Colors.black, fontSize: 14),
+            ),
+          ),
+          SizedBox(width: size.width / 20),
+          Text(
+            label,
+            style:
+                TextStyle(fontFamily: 'opensan', fontWeight: FontWeight.w700),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildWeaknessContainer() {
+    return Container(
+      width: containerWidth,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [Color(0xFF6A0DAD), Color(0xFF8A2BE2)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildWeakPoint(Icons.sentiment_very_satisfied, "Great"),
+          buildWeakPoint(Icons.person, "Weak"),
+        ],
+      ),
+    );
+  }
+
+  Widget buildWeakPoint(IconData icon, String label) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 30,
+          backgroundColor: Colors.white24,
+          child: Icon(icon, size: 30, color: Colors.white),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 16,
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
 
-class _ChartData {
-  _ChartData(this.x, this.y);
-
-  final String x;
-  final double y;
-}
+const double containerWidth = 300;
+const double containerHeight = 200;
