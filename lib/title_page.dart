478095a8d2307f735965d0901a0cbff6a3d42bd5
Staheos <staslewy@gmail.com>
2025-04-06 10:04:47 +0200

    font fix

--- lib/title_page.dart
+++ lib/title_page.dart
@@ -28,7 +28,9 @@ class _TitlePageState extends State<TitlePage>
       duration: const Duration(seconds: 5),
       vsync: this,
     );
+    // )..repeat(reverse: false);
     _animation = Tween<double>(begin: 0.0, end: 2.0).animate(_controller);
+
     initData();
   }
 
@@ -60,47 +62,46 @@ class _TitlePageState extends State<TitlePage>
     return Scaffold(
       body: Center(
         child: Container(
-          margin: EdgeInsets.symmetric(horizontal: size.width / 10),
+          margin: EdgeInsets.only(
+            left: size.width / 10,
+            right: size.width / 10,
+          ),
           child: Stack(
             children: <Widget>[
               Align(
-                alignment: const Alignment(0, -0.2),
-                // Wrap the Welcome text with RotationTransition animated by _animation
-                child: RotationTransition(
-                  turns: _animation,
-                  child: ShaderMask(
-                    shaderCallback: (bounds) => LinearGradient(
-                      colors: [const Color(0xffA7E2FF), const Color(0xffDC92FF)],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ).createShader(bounds),
-                    child: const Text(
-                      "Welcome",
-                      style: TextStyle(
-                        fontFamily: 'OleoScriptSwashCaps',
-                        fontSize: 60.0,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                      ),
+                alignment: Alignment(0, -0.2),
+                child: ShaderMask(
+                  shaderCallback: (bounds) => LinearGradient(
+                    colors: [Color(0xffA7E2FF), Color(0xffDC92FF)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ).createShader(bounds),
+                  child: Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontFamily: 'OleoScriptSwashCaps',
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ),
               Align(
-                alignment: const Alignment(0, -0.05),
-                child: const Text(
+                alignment: Alignment(0, -0.05),
+                child: Text(
                   "Prepare for your SAT exam the smart way!",
                   style: TextStyle(
                     fontFamily: 'Open',
                     fontSize: 20.0,
-                    fontWeight: FontWeight.w500,
+                    fontWeight: FontWeight.w600,
                     color: Colors.white,
                   ),
                   textAlign: TextAlign.center,
                 ),
               ),
               Align(
-                alignment: const Alignment(0, 0.81),
+                alignment: Alignment(0, 0.81),
                 child: SizedBox(
                   height: size.height * 0.06,
                   width: size.width * 0.72,
@@ -117,6 +118,8 @@ class _TitlePageState extends State<TitlePage>
                         if (snapshot.connectionState ==
                             ConnectionState.waiting) {
                           return const CircularProgressIndicator();
+                        } else if (prefs.getString('skill') == 'sats') {
+                          return const Home();
                         } else {
                           return const Home();
                         }
@@ -124,20 +127,19 @@ class _TitlePageState extends State<TitlePage>
                     ),
                     text: 'Continue',
                     width: size.width,
-                    color: const Color(0xff4c2f65),
+                    color: Color(0xff4c2f65),
                   ),
                 ),
               ),
               Align(
-                alignment: const Alignment(0, 0.93),
+                alignment: Alignment(0, 0.93),
                 child: RichText(
                   textAlign: TextAlign.center,
                   text: TextSpan(
                     style: Theme.of(context).textTheme.bodyMedium,
                     children: <TextSpan>[
                       TextSpan(
-                        text:
-                        "By using our App, you agree to our\n",
+                        text: "By using our App, you agree to our\n",
                         style: TextStyle(
                           fontSize: 0.015 * size.height,
                           color: Theme.of(context)
@@ -161,8 +163,7 @@ class _TitlePageState extends State<TitlePage>
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (context) =>
-                                const TermsOfService(),
+                                builder: (context) => const TermsOfService(),
                               ),
                             );
                           },
