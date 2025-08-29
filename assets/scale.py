3eb4aadc1a4bb469e85ee70df1c219f033e57c2f
Staheos <staslewy@gmail.com>
2025-08-29 14:11:59 +0200

    removed assets 2

--- assets/scale.py
+++
@@ -1,7 +0,0 @@
-import os
-from PIL import Image
-for f in os.listdir():
-	if f.lower().endswith((".png",".jpg",".jpeg",".bmp",".gif",".tiff",".webp")):
-		i=Image.open(f)
-		i=i.resize((i.width//2,i.height//2),Image.LANCZOS)
-		i.save(f)
